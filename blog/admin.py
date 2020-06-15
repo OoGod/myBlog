674d5bb41179aee080da558755c644ede7d5e4f6
OoGod <1617605689@qq.com>
2020-06-15 12:14:25 +0800

    'update'

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Article, Author, Tag, Category
+from .models import Article, Author
+# from .models import Article, Author, Tag, Category
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Author)
-admin.site.register(Tag)
-admin.site.register(Category)
+# admin.site.register(Tag)
+# admin.site.register(Category)
